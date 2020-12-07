366a84723d409cc0886ee2492bb47412336245df
”Kirill <zobov.kirill21200@mail.ru>
2020-12-07 23:25:51 +0700

    add lab5 as lab6

---
+++ Lab6/Lab6/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.debugLabel = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.TextBox();
+            this.regPassword = new System.Windows.Forms.TextBox();
+            this.regLogin = new System.Windows.Forms.TextBox();
+            this.Create = new System.Windows.Forms.Button();
+            this.Enter = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.regPassword);
+            this.groupBox1.Controls.Add(this.Create);
+            this.groupBox1.Controls.Add(this.regLogin);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(39, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 279);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Регистрация";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Enter);
+            this.groupBox2.Controls.Add(this.password);
+            this.groupBox2.Controls.Add(this.login);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(300, 35);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(181, 279);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Авторизация";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Пароль";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Пароль";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Логин";
+            // 
+            // debugLabel
+            // 
+            this.debugLabel.AutoSize = true;
+            this.debugLabel.Location = new System.Drawing.Point(36, 332);
+            this.debugLabel.Name = "debugLabel";
+            this.debugLabel.Size = new System.Drawing.Size(83, 17);
+            this.debugLabel.TabIndex = 4;
+            this.debugLabel.Text = "debugLabel";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(6, 148);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(169, 22);
+            this.password.TabIndex = 5;
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(6, 66);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(169, 22);
+            this.login.TabIndex = 6;
+            // 
+            // regPassword
+            // 
+            this.regPassword.Location = new System.Drawing.Point(6, 148);
+            this.regPassword.Name = "regPassword";
+            this.regPassword.Size = new System.Drawing.Size(188, 22);
+            this.regPassword.TabIndex = 7;
+            // 
+            // regLogin
+            // 
+            this.regLogin.Location = new System.Drawing.Point(9, 66);
+            this.regLogin.Name = "regLogin";
+            this.regLogin.Size = new System.Drawing.Size(185, 22);
+            this.regLogin.TabIndex = 8;
+            // 
+            // Create
+            // 
+            this.Create.Location = new System.Drawing.Point(5, 191);
+            this.Create.Name = "Create";
+            this.Create.Size = new System.Drawing.Size(109, 40);
+            this.Create.TabIndex = 7;
+            this.Create.Text = "Регистрация";
+            this.Create.UseVisualStyleBackColor = true;
+            this.Create.Click += new System.EventHandler(this.Create_Click);
+            // 
+            // Enter
+            // 
+            this.Enter.Location = new System.Drawing.Point(6, 191);
+            this.Enter.Name = "Enter";
+            this.Enter.Size = new System.Drawing.Size(93, 40);
+            this.Enter.TabIndex = 8;
+            this.Enter.Text = "Вход";
+            this.Enter.UseVisualStyleBackColor = true;
+            this.Enter.Click += new System.EventHandler(this.Enter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 383);
+            this.Controls.Add(this.debugLabel);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox regPassword;
+        private System.Windows.Forms.Button Create;
+        private System.Windows.Forms.TextBox regLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button Enter;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox login;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label debugLabel;
+    }
+}
+
---
+++ Lab6/Lab6/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string GetMD5(string value)
+        {
+            var provider = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            var data = Encoding.UTF8.GetBytes(value);
+            data = provider.ComputeHash(data);
+            return Encoding.UTF8.GetString(data);
+        }
+
+        private void Create_Click(object sender, EventArgs e)
+        {
+            var db = new DataClasses1DataContext();
+            var query = from c in db.Users
+                        where (c.Login == regLogin.Text)
+                        select c;
+
+            if (query.Count() == 0)
+            {
+                if (!(String.IsNullOrWhiteSpace(regPassword.Text)) && !(String.IsNullOrWhiteSpace(regLogin.Text)))
+                {
+                    if (regPassword.Text.Contains(regPassword.Text.FirstOrDefault(c => Char.IsLower(c)))
+                        && regPassword.Text.Contains(regPassword.Text.FirstOrDefault(c => Char.IsUpper(c)))
+                        && regPassword.Text.Contains(regPassword.Text.FirstOrDefault(c => Char.IsDigit(c))))
+                    {
+                        var hash = GetMD5(regPassword.Text);
+                        debugLabel.Text = hash;
+
+                        var data = new Users
+                        {
+                            Login = regLogin.Text,
+                            Password = hash
+                        };
+
+                        db.Users.InsertOnSubmit(data);
+                        db.SubmitChanges();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Придумайте другой пароль", "Логин и пароль");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Заполните все поля", "Логин и пароль");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Такой логин уже используется", "Логин и пароль");
+            }
+        }
+
+        private void Enter_Click(object sender, EventArgs e)
+        {
+            var db = new DataClasses1DataContext();
+            var hash = GetMD5(password.Text);
+
+            var query = from c in db.Users
+                        where (c.Login == login.Text && c.Password == hash)
+                        select c;
+
+            if (query.Count() == 1)
+            {
+                MessageBox.Show("Найдено совпадение!", "Логин и пароль");
+            }
+            else
+            {
+                MessageBox.Show("Совпадений не найдено!", "Логин и пароль");
+            }
+        }
+    }
+}
